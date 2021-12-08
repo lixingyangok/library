9a6738c4c40bd85aed233d419597711c63a1f5e0
lixingyang <176840078@qq.com>
2021-12-08 21:00:14 +0700

    探索 sqlite 用法

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import store from './store/store.js';
 import './common/style/minireset.css';
 import './common/style/global.scss';
 
+const {exec} = require('child_process'); 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('D:/myDB.db');
-const {exec} = require('child_process'); 
+
 
 // ▼查询 exe 位置
 // const { remote } = require('electron');
@@ -20,15 +21,14 @@ const {exec} = require('child_process');
 // console.log('__dirname\n', __dirname);
 
 const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-for (var i = 0; i < 2; i++) {
+for (var i = 0; i < 3; i++) {
     stmt.run(new Date().toLocaleString());
 }
 stmt.finalize();
-let iQuantity = 0;
-db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-    iQuantity++;
+// db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+db.each("SELECT count(*) FROM lorem", function(err, row) {
+    console.log('数据库数量：', row);
 });
-console.log('数据库数量：', iQuantity);
 
 // db.serialize(function() {
 //     db.run("CREATE TABLE lorem (info TEXT)");
